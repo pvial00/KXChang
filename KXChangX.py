681befb3802c9c64ab6a7a77e45d4506032d90e8
Karl Zander <pvial@Karls-MacBook-Air.local>
2019-11-06 09:52:07 -0800

    KXChang

--- KXChangX.py
+++ KXChangX.py
@@ -10,19 +10,22 @@ def genBase(size):
 
 def keygen(size):
     A, B = genBase(size)
-    sk = number.getRandomRange(1, (B - 1))
-    return sk, B, A
+    N = A * B
+    sk = number.getRandomRange(1, (N - 1))
+    return sk, B, N
 
 def kxchang_demo(size):
     # Generate keys for both parties
     skA, pkA, nA = keygen(size)
     skB, pkB, nB = keygen(size)
+    S = nA * nB
+    
     # Generate phase 1 by encrypting the public key
-    phase1A = pow(pkA, skA, nA)
-    phase1B = pow(pkA, skB, nA)
+    phase1A = pow(pkA, skA, S)
+    phase1B = pow(pkA, skB, S)
     # Generate the phase 2 secret modulus
-    phase2A = pow(phase1B, skA, nA)
-    phase2B = pow(phase1A, skB, nA)
+    phase2A = pow(phase1B, skA, S)
+    phase2B = pow(phase1A, skB, S)
     # Both parties encrypt y using the secret modulus and send phase3
     phase3A = pow(pkB, skA, phase2A)
     phase3B = pow(pkB, skB, phase2B)
